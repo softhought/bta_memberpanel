72a392111544332b868d32734808c2455e97b2ba
Suman Dey <sumandey7689@gmail.com>
2025-06-23 12:15:48 +0530

    bta

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        Commands\ProcessTaskCommand::class
     ];
 
     protected function schedule(Schedule $schedule)
