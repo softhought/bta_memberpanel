e528f74098864f2a621bc80ba780c23cae794413
Suman Dey <sumandey7689@gmail.com>
2025-07-04 13:34:25 +0530

    webhook

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,17 +9,23 @@ class DeployController extends Controller
     public function pullCode()
     {
         $branch = 'development';
-
         $path = base_path();
 
-        $process = new Process(['git', 'pull']);
-        $process->setWorkingDirectory($path);
+        // Tell Git to use a specific SSH key
+        $sshCommand = "ssh -i /usr/share/httpd/bta/.ssh/id_rsa -o StrictHostKeyChecking=no";
+
+        // Set GIT_SSH_COMMAND so it uses the custom key
+        $process = Process::fromShellCommandline("git pull origin $branch", $path, [
+            'GIT_SSH_COMMAND' => $sshCommand,
+        ]);
+
         $process->run();
 
         if (!$process->isSuccessful()) {
             return response()->json([
                 'status' => 'error',
-                'output' => $process->getErrorOutput()
+                'output' => $process->getErrorOutput(),
+                'process' => null
             ], 500);
         }
 
