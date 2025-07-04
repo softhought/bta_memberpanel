00c12055920c8dfe8d69aede1b8c2ffd5b5196b8
Suman Dey <sumandey7689@gmail.com>
2025-07-04 12:52:08 +0530

    webhook

---
+++ app/Http/Controllers/DeployController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class DeployController extends Controller
+{
+    public function pullCode()
+    {
+        $branch = 'development';
+
+        $path = base_path();
+
+        $process = new Process(['git', 'pull', 'origin', $branch]);
+        $process->setWorkingDirectory($path);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return response()->json([
+                'status' => 'error',
+                'output' => $process->getErrorOutput()
+            ], 500);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'output' => $process->getOutput()
+        ]);
+    }
+}
