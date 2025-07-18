b9a350ccbd8094506fe2242b3d864282a5a8816c
Suman Dey <sumandey7689@gmail.com>
2025-07-18 12:54:12 +0530

    payment

--- app/Models/MemberReceiptDetail.php
+++ app/Models/MemberReceiptDetail.php
@@ -8,6 +8,27 @@ class MemberReceiptDetail extends BaseModel
 {
     use HasFactory;
     protected $table = 'member_receipt_details';
+    protected $fillable = [
+        'receipt_master_id',
+        'year',
+        'month_id',
+        'cr_ac_id',
+        'component_id',
+        'item_amount',
+        'item_qty',
+        'amount',
+        'discount',
+        'taxable_amount',
+        'cgst_id',
+        'sgst_id',
+        'cgst_amount',
+        'sgst_amount',
+        'total_gst_amount',
+        'is_payment_due',
+        'due_amount',
+        'net_amount',
+        'is_waiver',
+    ];
 
     public function receipt()
     {
