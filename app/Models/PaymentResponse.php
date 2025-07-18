78525aaa6aed48e2d51d62ffd7ab2c637e345fe8
Suman Dey <sumandey7689@gmail.com>
2025-07-18 11:48:34 +0530

    payment

--- app/Models/PaymentResponse.php
+++ app/Models/PaymentResponse.php
@@ -8,4 +8,15 @@ class PaymentResponse extends BaseModel
 {
     use HasFactory;
     protected $table = 'payment_response';
+    protected $fillable = [
+        'transaction_id',
+        'order_id',
+        'payment_status',
+        'processing_date',
+        'tracking_id',
+        'bank_ref_no',
+        'payment_geteway',
+        'response_data',
+        'payment_message'
+    ];
 }
