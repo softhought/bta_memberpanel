ac07ab52e5976ef368c65a4920971af36c174b31
Suman Dey <sumandey7689@gmail.com>
2025-08-04 14:08:28 +0530

    bank charges error

--- app/Helpers/AdminHelper.php
+++ app/Helpers/AdminHelper.php
@@ -696,8 +696,7 @@ function processPendingPayments()
 
                 $sessionData = json_decode($value->payment_session_data, true);
 
-                $bankCharges = (float) $response['amount'] - (float) $sessionData['payable'];
-                echo $sessionData['payable'];exit;
+                $bankCharges = (float) $response['amount'] - (float) array_sum($sessionData['payable']);
                 processPayment($sessionData, $value, $bankCharges);
             } else {
                 PaymentRequest::where('id', $value->id)->update(['is_checking' => 'Y']);
