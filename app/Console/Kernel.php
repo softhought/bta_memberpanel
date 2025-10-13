9a23459c383450ff4a091221e83249be4906eec7
Suman Dey <sumandey7689@gmail.com>
2025-10-13 16:02:03 +0530

    pending payments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('app:process-pending-payments')->hourly();
-        $schedule->command('app:process-pending-payments-all')->everyMinute();
+        $schedule->command('app:process-pending-payments-all')->dailyAt('23:50');
     }
 
     /**
